1db3d6d4b26a64789f05c7d96a575122f7fd0898
pilipala <pilipala@pilipaladeMacBook-Pro.local>
2022-04-12 15:39:49 +0800

    init project

---
+++ TRULogining/Classes/GQLogin.h
@@ -0,0 +1,16 @@
+//
+//  GQLogin.h
+//  TRULogining
+//
+//  Created by pilipala on 2022/4/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GQLogin : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRULogining/Classes/GQLogin.m
@@ -0,0 +1,12 @@
+//
+//  GQLogin.m
+//  TRULogining
+//
+//  Created by pilipala on 2022/4/12.
+//
+
+#import "GQLogin.h"
+
+@implementation GQLogin
+
+@end
